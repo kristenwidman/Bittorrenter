5e01af9d0125d1456c60acb2e910dff3a479e887
kwidman <kristenwidman@gmail.com>
2012-10-24 16:52:17 -0400

    working again. moved peer related functionality back to bittorrent.py BittorrentProtocol and out of ActiveTorrent. ActiveTorrent will be used for things related to the full torrent.

--- active_torrent.py
+++ active_torrent.py
@@ -4,101 +4,11 @@ from pieces import *
 from messages import *
 from bitstring import BitArray
 
-max_requests = 15
-
 class ActiveTorrent(object):
     def __init__(self, torrent_info):
         self.torrent_info = torrent_info
-        self.message_buffer = bytearray()
-        self.interested = False
-        self.peer_interested = False
-        self.choked = True
-        self.peer_choked = True
-        self.peer_bitarray = BitArray(len(torrent_info.pieces_array)) 
-        self.pending_requests = 0
         self.piece_downloading = MyPiece(self.torrent_info.piece_length)  #clear and restart when creating a new piece
-        self.piece_number = 0   #increment this when creating a new piece?
-        print 'number of blocks for this piece: ' + str(self.piece_downloading.block_number)
+        self.piece_number = 0   #increment this (by 8?) when creating a new piece?
         self.requested_blocks = BitArray(self.piece_downloading.block_number)
         self.have_blocks = BitArray(self.piece_downloading.block_number)
 
-    def deal_with_message(self,data):
-        messages_to_send_list = []
-        if self.message_buffer:
-            self.message_buffer.extend(bytearray(data))
-            #print "message buffer had info. it is now: " + repr(self.message_buffer)
-        else:
-            self.message_buffer = bytearray(data)
-            #print "message buffer was empty.  it is now: " + repr(self.message_buffer)
-        if self.message_buffer[1:20].lower() == "BitTorrent Protocol".lower():
-            print "handshake received"
-            self.message_buffer = self.decode_handshake(self.message_buffer, self.torrent_info)
-            messages_to_send_list.append(repr(Interested()))
-            self.interested = True
-            #perhaps have error handling for if handshake is cut short
-        if len(self.message_buffer) >= 4:
-            message_length = bytes_to_number(self.message_buffer[0:4]) + 4
-            #print "length of message expected: " + repr(message_length)
-            #print "length of message in buffer: " + repr(len(self.message_buffer))
-            if len(self.message_buffer) < message_length:    #debugging line
-                print "message shorter than expected"        #debugging line
-            while len(self.message_buffer) >= message_length:
-                self.message_buffer, messages_to_send_list = self.parse_messages(messages_to_send_list)
-                #print "message_buffer is now: " + repr(self.message_buffer) 
-        return messages_to_send_list
-
-    def get_next_request(self):
-        #get next request
-        pass  #use self.peer_bitarray and self.torrent_info
-
-    def parse_messages(self, messages_to_send_list):
-        message_obj, message = parse_message_from_response(self.message_buffer)
-        print 'message type: ' + repr(type(message_obj))
-        if isinstance(message_obj, Choke):
-            self.choked = True
-        if isinstance(message_obj, Unchoke):
-            self.choked = False
-            if self.interested:
-                #messages_to_send_list.append(self.get_next_request()
-                pass
-        if isinstance(message_obj, Interested):
-            self.peer_interested = True
-        if isinstance(message_obj, NotInterested):
-            self.peer_interested = False
-        if isinstance(message_obj, Have):
-            piece_index = message_obj.index
-            print 'piece index: ' + repr(piece_index)
-            piece_index = bytes_to_number(piece_index)
-            self.peer_bitarray[piece_index] = 1
-            print 'bitarray after have: ' + repr(self.peer_bitarray.bin)
-            #request = self.get_next_request() #will need bitarray and torrent_info
-        if isinstance(message_obj, Bitfield):
-            bitarray = BitArray(bytes=message_obj.bitfield)
-            self.peer_bitarray = bitarray[:len(self.peer_bitarray)]
-            print 'self.peer_bitarray: ' + repr(self.peer_bitarray)
-#send request
-        if isinstance(message_obj, Request):
-            #if !self.peer_choked && self.peer_interested:
-                #send piece
-            pass
-        if isinstance(message_obj, Piece):
-            print 'piece'    # do something with this
-#send request
-        if isinstance(message_obj, Cancel):
-            print 'cancel'
-        if isinstance(message_obj, Port):
-            print 'port'
-            #parse port and switch connection to that port
-        return message, messages_to_send_list
-
-
-    def decode_handshake(self, response, torrentObj):
-        handshake = Handshake(response)
-        other = response[68:]
-        expected_peer_id = torrentObj.peer_id
-        expected_info_hash = torrentObj.info_hash
-        if (expected_info_hash != handshake.info_hash):
-            #instead of throwing exception, we should send a cancel message
-            raise Exception('info_hash does not match expected. Info hash expected: ' +
-                            repr(expected_info_hash) + '. Info hash found: ' + repr(handshake.info_hash))
-        return other
--- bittorrenter.py
+++ bittorrenter.py
@@ -14,13 +14,25 @@ from twisted.internet import reactor
 from torrent import Torrent
 from active_torrent import ActiveTorrent
 from messages import *
+from pieces import *
+from bitstring import BitArray
 #from sys import stdout
+
+max_requests = 15
+request_length = 2**14
 
 class BittorrentProtocol(Protocol):
     def __init__(self,torrent_info):
         #super(BittorrentProtocol, self).__init__()
         #Protocol.__init__(self)
         self.torrent_info = torrent_info
+        self.peer_has_pieces = BitArray(len(torrent_info.pieces_array)) 
+        self.pending_requests = 0
+        self.message_buffer = bytearray()
+        self.interested = False
+        self.peer_interested = False
+        self.choked = True
+        self.peer_choked = True
         self.active_torrent = ActiveTorrent(torrent_info)
 
     def connectionMade(self):
@@ -29,10 +41,100 @@ class BittorrentProtocol(Protocol):
         #self.transport.loseConnection()
 
     def dataReceived(self,data):
-        messages_to_send = self.active_torrent.deal_with_message(data)
+        messages_to_send = self.deal_with_message(data)
         for message in messages_to_send:
             self.transport.write(message)
-        
+
+    def deal_with_message(self,data):
+        messages_to_send_list = []
+        if self.message_buffer:
+            self.message_buffer.extend(bytearray(data))
+            #print "message buffer had info. it is now: " + repr(self.message_buffer)
+        else:
+            self.message_buffer = bytearray(data)
+            #print "message buffer was empty.  it is now: " + repr(self.message_buffer)
+        if self.message_buffer[1:20].lower() == "BitTorrent Protocol".lower():
+            print "handshake received"
+            self.message_buffer = self.decode_handshake(self.message_buffer, self.torrent_info)
+            messages_to_send_list.append(repr(Interested()))
+            self.interested = True
+            #perhaps have error handling for if handshake is cut short
+        if len(self.message_buffer) >= 4:
+            message_length = bytes_to_number(self.message_buffer[0:4]) + 4
+            #print "length of message expected: " + repr(message_length)
+            #print "length of message in buffer: " + repr(len(self.message_buffer))
+            if len(self.message_buffer) < message_length:    #debugging line
+                print "message shorter than expected"        #debugging line
+            while len(self.message_buffer) >= message_length:
+                self.message_buffer, messages_to_send_list = self.parse_messages(messages_to_send_list)
+                #print "message_buffer is now: " + repr(self.message_buffer) 
+        return messages_to_send_list
+
+    def get_next_request(self):
+        #get next request
+        for i in range(len(self.have_blocks)): #should jump by request_length rather than every one
+            if self.peer_has_pieces[active_torrent.piece_number] & active_torrent.have_blocks[i]==0 & active_torrent.requested_blocks[i]==0:
+                print 'peer has piece ' + str(active_torrent.piece_number) + ' and I do not have and have not requested block ' + str(i)
+                if self.pending_requests <= max_requests:
+                    print 'creating a request object'
+                    request = Request(index=active_torrent.piece_number, begin=i, length=request_length)
+                    print 'request object created: ' + repr(request)
+                    return repr(request)
+        #pass  #use self.peer_has_pieces and self.torrent_info
+
+    def parse_messages(self, messages_to_send_list):
+        message_obj, message = parse_message_from_response(self.message_buffer)
+        print 'message type: ' + repr(type(message_obj))
+        if isinstance(message_obj, Choke):
+            self.choked = True
+        if isinstance(message_obj, Unchoke):
+            self.choked = False
+            if self.interested:
+                #messages_to_send_list.append(self.get_next_request()
+                pass
+        if isinstance(message_obj, Interested):
+            self.peer_interested = True
+        if isinstance(message_obj, NotInterested):
+            self.peer_interested = False
+        if isinstance(message_obj, Have):
+            piece_index = message_obj.index
+            print 'piece index: ' + repr(piece_index)
+            piece_index = bytes_to_number(piece_index)
+            self.peer_has_pieces[piece_index] = 1
+            print 'bitarray after have: ' + repr(self.peer_has_pieces.bin)
+            print 'bitarray of index 2: ' + repr(self.peer_has_pieces[2]) + ' ' + repr(self.peer_has_pieces[1]) + ' ' + repr(self.peer_has_pieces[3]) + ' ' + repr(self.peer_has_pieces[4])
+            #request = self.get_next_request() #will need bitarray and torrent_info
+        if isinstance(message_obj, Bitfield):
+            bitarray = BitArray(bytes=message_obj.bitfield)
+            self.peer_has_pieces = bitarray[:len(self.peer_has_pieces)]
+            print 'self.peer_has_pieces: ' + repr(self.peer_has_pieces)
+#send request
+        if isinstance(message_obj, Request):
+            #if !self.peer_choked && self.peer_interested:
+                #send piece
+            pass
+        if isinstance(message_obj, Piece):
+            print 'piece'    # do something with this
+#send request
+        if isinstance(message_obj, Cancel):
+            print 'cancel'
+        if isinstance(message_obj, Port):
+            print 'port'
+            #parse port and switch connection to that port
+        return message, messages_to_send_list
+
+
+    def decode_handshake(self, response, torrentObj):
+        handshake = Handshake(response)
+        other = response[68:]
+        expected_peer_id = torrentObj.peer_id
+        expected_info_hash = torrentObj.info_hash
+        if (expected_info_hash != handshake.info_hash):
+            #instead of throwing exception, we should send a cancel message
+            raise Exception('info_hash does not match expected. Info hash expected: ' +
+                            repr(expected_info_hash) + '. Info hash found: ' + repr(handshake.info_hash))
+        return other
+
 class BittorrentFactory(ClientFactory):
     def __init__(self,torrent_info):
         #super(BittorrentFactory, self).__init__()
@@ -135,7 +237,7 @@ def main(torrentFile):
     peers, torrent_obj = get_peers(metainfo)
 
     print peers
-    peer = peers[1]
+    peer = peers[0]
     #print peer
     hostandport = peer.split(':')
     #other = decode_handshake(response, torrentObj)
